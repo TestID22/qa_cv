2a9ac6ed38a569b7fb2bade41baf9aa7a4e1f0b3
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-04 12:38:32 +0300

    added and configured - index.html template: Settings - accept templates urls - configured path to maing page

--- qa_cv/qa_cv/settings.py
+++ qa_cv/qa_cv/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os.path
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainpage'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'qa_cv.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'template')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
--- qa_cv/qa_cv/urls.py
+++ qa_cv/qa_cv/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from mainpage.views import mainPageView
 
 urlpatterns = [
+    path('', mainPageView, name='mainpage'),
     path('admin/', admin.site.urls),
 ]
