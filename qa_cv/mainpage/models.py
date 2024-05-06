bc7b7c84ae0145074830dd8b4f4e4ba788f7b038
vitaliiSeptemner <vitaliiseptember@gmail.com>
2024-05-06 11:30:00 +0300

    admin: index:html: model: POST = rewritten __str__ styles.css: margin padding = 0

--- qa_cv/mainpage/models.py
+++ qa_cv/mainpage/models.py
@@ -3,4 +3,8 @@ from django.db import models
 # Create your models here.
 class Post(models.Model):
 
-    post = models.CharField(max_length=38)
+    post = models.CharField(max_length=38, verbose_name="POST")
+
+
+    def __str__(self):
+        return self.post
